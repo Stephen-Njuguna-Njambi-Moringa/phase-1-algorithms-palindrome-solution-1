9b2c6378aa11bd8b8865cc45cd7b8b2a2530e804
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-09-07 20:08:16 +0300

    lab passed

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var reverseWord = word.split("").reverse().join('')
+  if (reverseWord == word){
+      return true
+  }
+  else if(word.split('').length<2){
+      return false
+
+  }
+  else{
+      return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  
+  reverse the input string
+  if the reversed string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -18,8 +36,22 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
